537c53d106d9682c7cbcbe8fb7fe659ae9a09c23
talenaa <talenabarbosa7@gmail.com>
2024-12-17 15:59:28 +0100

    style: finished the style for the home view

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,26 @@
 <footer>
     <p>© 2024 Santa's Toy Factory | HO HO HO! 🎅</p>
+    <a href="https://github.com/sr-calcetines" target="_blank" class="git">
+        <img src="{{ asset('css/img/git.png') }}" alt="GitHub Profile" class="git">
+    </a>
+
+    <a href="https://github.com/sr-calcetines" target="_blank" class="git">
+        <img src="{{ asset('css/img/git.png') }}" alt="GitHub Profile" class="git">
+    </a>
+
+    <a href="https://github.com/sr-calcetines" target="_blank" class="git">
+        <img src="{{ asset('css/img/git.png') }}" alt="GitHub Profile" class="git">
+    </a>
+
+    <a href="https://github.com/sr-calcetines" target="_blank" class="git">
+        <img src="{{ asset('css/img/git.png') }}" alt="GitHub Profile" class="git">
+    </a>
+
+    <a href="https://github.com/sr-calcetines" target="_blank" class="git">
+        <img src="{{ asset('css/img/git.png') }}" alt="GitHub Profile" class="git">
+    </a>
+
+    <a href="https://github.com/sr-calcetines" target="_blank" class="git">
+        <img src="{{ asset('css/img/git.png') }}" alt="GitHub Profile" class="git">
+    </a>
 </footer>
